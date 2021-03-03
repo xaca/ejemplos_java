3de872e4abc02ec02903f9803cd34a37425ae950
xaca <and1res@hotmail.com>
2021-03-03 14:41:12 -0500

    Ejemplo promedio

---
+++ ejemplo_04/Promedio.java
@@ -0,0 +1,13 @@
+public class Promedio{
+	public static void main(String[] args) {
+		float resultado, dato1 = 2;
+		int dato2 = 4;
+		byte dato3 = 127;
+		short dato4 = 456;
+		char dato5 = 6;
+
+		resultado = (dato1+dato2+dato3+dato4+dato5)/5;
+
+		System.out.println("El resultado es "+resultado);
+	}
+}
