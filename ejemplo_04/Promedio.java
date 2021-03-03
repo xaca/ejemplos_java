c891868d4f5a27a5dd0e0a3b268cf1708b1b74bf
xaca <and1res@hotmail.com>
2021-03-03 15:32:38 -0500

    Ejemplo de clase

--- ejemplo_04/Promedio.java
+++ ejemplo_04/Promedio.java
@@ -1,5 +1,6 @@
 public class Promedio{
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		float resultado, dato1 = 2;
 		int dato2 = 4;
 		byte dato3 = 127;
@@ -8,6 +9,29 @@ public class Promedio{
 
 		resultado = (dato1+dato2+dato3+dato4+dato5)/5;
 
+		//System.out.println("El resultado es "+resultado);
+
+		/* Como imprimir los valores de las variables en un nuevo reglon
+		y por ultimo el resultado*/
+
+		System.out.println();
+		System.out.print("float "+dato1);		
+		System.out.print(" int "+dato2);		
+		System.out.print(" byte "+dato3);		
+		System.out.print(" short "+dato4);		
+		System.out.print(" char "+dato5);
+		System.out.println();
 		System.out.println("El resultado es "+resultado);
+		System.out.println();
+		System.out.println("float "+dato1);		
+		System.out.println("int "+dato2);		
+		System.out.println("byte "+dato3);		
+		System.out.println("short "+dato4);		
+		System.out.println("char "+dato5);
+		System.out.println("El resultado es "+resultado);
+		System.out.println();
+		/* Como imprimir los valores de las variables en el mismo reglon
+		y por ultimo el resultado */
+
 	}
 }
---
+++ ejemplo_05/Clasificacion.java
@@ -0,0 +1,13 @@
+public class Clasificacion{
+	public static void main(String[] args) {
+		/* 
+
+		   Programa que declare e inicialice 10 numeros
+		   y clasifique asi: cuantos son negativos, cuantos
+		   menores a 100 y cuantos mayores a 5000.
+
+		   extra: Cuantos son pares y cuantos impares
+
+		*/
+	}
+}
