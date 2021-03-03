f00a0cb8794f85dead2029f6c46ad52760fd40f3
xaca <and1res@hotmail.com>
2021-03-03 14:14:26 -0500

    Actualizacion ejemplos de clase

---
+++ ejemplo_02/Variables.java
@@ -0,0 +1,16 @@
+public class Variables{
+	public static void main(String[] args) {
+		byte dia; //Declaración
+		byte a,b,c; //Declaración
+		int d;
+
+		dia = 127; //Inicialización
+		a = 60;
+		b = 40;
+
+		d = a+b;
+
+		//System.out.println("El dia es "+dia);
+		System.out.println("La suma es "+d);//Pendiente variación
+	}
+}
